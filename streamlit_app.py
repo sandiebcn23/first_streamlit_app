7d1a6c7142f9e140571bad712e2019e8195b289b
sandiebcn23 <126023504+sandiebcn23@users.noreply.github.com>
2023-05-03 11:03:23 +0200

    Update streamlit_app.py
    menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.title('Omega 3 and Blueberry Oatmeal')
+streamlit.title('Kale, SPinach and Rocket Smoothie')
+streamlit.title('Hard Boiled FRee Range Eggs')
